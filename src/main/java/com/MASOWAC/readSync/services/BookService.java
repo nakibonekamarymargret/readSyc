2ab33419ea4a0e3888bae505d92fe50202037913
704KC <nakibonekamarymargret@gmail.com>
2025-03-25 15:50:07 +0300

    Added service  and controller  classes for the book module

--- src/main/java/com/MASOWAC/readSync/services/BookService.java
+++ src/main/java/com/MASOWAC/readSync/services/BookService.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -131,7 +132,7 @@ public class BookService {
             }
 
             // Throw a custom exception with the generated message
-            throw new BookNotFoundException(message);
+            throw new NoBookFoundException(message);
         }
 
         return books;
